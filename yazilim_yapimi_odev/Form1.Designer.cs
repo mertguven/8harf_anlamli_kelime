13f749bdd51721f11051401d12234b3456a3770a
Mert <45696773+mertguven@users.noreply.github.com>
2020-04-15 16:09:09 +0300

    Proje dosyası ekle.

---
+++ yazilim_yapimi_odev/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace yazilim_yapimi_odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rastgeleBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.harflerTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.kelimeEkleBtn = new System.Windows.Forms.Button();
+            this.veriCekBtn = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listeleBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.yeniOyunBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rastgeleBtn
+            // 
+            this.rastgeleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rastgeleBtn.Location = new System.Drawing.Point(31, 426);
+            this.rastgeleBtn.Name = "rastgeleBtn";
+            this.rastgeleBtn.Size = new System.Drawing.Size(240, 39);
+            this.rastgeleBtn.TabIndex = 0;
+            this.rastgeleBtn.Text = "Rastgele Oluştur";
+            this.rastgeleBtn.UseVisualStyleBackColor = true;
+            this.rastgeleBtn.Click += new System.EventHandler(this.rastgeleBtn_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(287, 251);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(337, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 18);
+            this.label2.TabIndex = 3;
+            // 
+            // harflerTxt
+            // 
+            this.harflerTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.harflerTxt.Location = new System.Drawing.Point(73, 334);
+            this.harflerTxt.Name = "harflerTxt";
+            this.harflerTxt.Size = new System.Drawing.Size(157, 22);
+            this.harflerTxt.TabIndex = 5;
+            this.harflerTxt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(118, 309);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Harfler";
+            // 
+            // kelimeEkleBtn
+            // 
+            this.kelimeEkleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kelimeEkleBtn.Location = new System.Drawing.Point(31, 372);
+            this.kelimeEkleBtn.Name = "kelimeEkleBtn";
+            this.kelimeEkleBtn.Size = new System.Drawing.Size(240, 39);
+            this.kelimeEkleBtn.TabIndex = 7;
+            this.kelimeEkleBtn.Text = "Kelimeleri Ekle";
+            this.kelimeEkleBtn.UseVisualStyleBackColor = true;
+            this.kelimeEkleBtn.Click += new System.EventHandler(this.kelimeEkleBtn_Click);
+            // 
+            // veriCekBtn
+            // 
+            this.veriCekBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.veriCekBtn.Location = new System.Drawing.Point(452, 308);
+            this.veriCekBtn.Name = "veriCekBtn";
+            this.veriCekBtn.Size = new System.Drawing.Size(227, 52);
+            this.veriCekBtn.TabIndex = 8;
+            this.veriCekBtn.Text = "Verileri Çek";
+            this.veriCekBtn.UseVisualStyleBackColor = true;
+            this.veriCekBtn.Click += new System.EventHandler(this.veriCekBtn_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(328, 403);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(460, 173);
+            this.listBox3.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(464, 372);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 22);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Veritabanı Ile Eşleşenler";
+            // 
+            // listeleBtn
+            // 
+            this.listeleBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listeleBtn.Location = new System.Drawing.Point(31, 481);
+            this.listeleBtn.Name = "listeleBtn";
+            this.listeleBtn.Size = new System.Drawing.Size(240, 39);
+            this.listeleBtn.TabIndex = 12;
+            this.listeleBtn.Text = "Eşleşenleri Listele";
+            this.listeleBtn.UseVisualStyleBackColor = true;
+            this.listeleBtn.Click += new System.EventHandler(this.listeleBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(518, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 22);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Veritabanı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(59, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(191, 22);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Oluşturulan Kelimeler";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(328, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(460, 251);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // yeniOyunBtn
+            // 
+            this.yeniOyunBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.yeniOyunBtn.Location = new System.Drawing.Point(31, 537);
+            this.yeniOyunBtn.Name = "yeniOyunBtn";
+            this.yeniOyunBtn.Size = new System.Drawing.Size(240, 39);
+            this.yeniOyunBtn.TabIndex = 17;
+            this.yeniOyunBtn.Text = "Yeni Oyun";
+            this.yeniOyunBtn.UseVisualStyleBackColor = true;
+            this.yeniOyunBtn.Click += new System.EventHandler(this.yeniOyunBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 598);
+            this.Controls.Add(this.yeniOyunBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listeleBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.veriCekBtn);
+            this.Controls.Add(this.kelimeEkleBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.harflerTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.rastgeleBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rastgeleBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox harflerTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button kelimeEkleBtn;
+        private System.Windows.Forms.Button veriCekBtn;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button listeleBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button yeniOyunBtn;
+    }
+}
+
---
+++ yazilim_yapimi_odev/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace yazilim_yapimi_odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        tdk_kelimeEntities ent = new tdk_kelimeEntities();//entity framework nesnesi oluşturuluyor
+        Random rastgele = new Random();
+        ArrayList eleman = new ArrayList();
+        char[] dizi = { 'a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ö', 'p', 'r', 's', 'ş', 't', 'u', 'ü', 'v', 'y', 'z' };
+        string metin = "";
+        char[] karakterler = new char[8];
+        string[] harfler = new string[8];
+        int harfyeri;
+        string x;
+        int r = 3;//Kombinasyon 3'ten başlayacak örn: 8'in 3'lüsü
+
+        private void rastgeleBtn_Click(object sender, EventArgs e)//Sayıların random atandığı yer
+        {
+            MessageBox.Show("Bu işlem biraz uzun sürebilir!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            harflerTxt.Text = "";
+            for (int i = 0; i < 8; i++) {
+                harfyeri = rastgele.Next(0, dizi.Length);
+                karakterler[i] = dizi[harfyeri];
+                harflerTxt.Text += karakterler[i].ToString();
+            }
+            OrtakIslem();
+        }
+
+        private void kelimeEkleBtn_Click(object sender, EventArgs e)//listbox1'e harflerin olabilecek bütün karmasını yazar
+        {
+            MessageBox.Show("Bu işlem biraz uzun sürebilir!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            metin = harflerTxt.Text;
+            karakterler = metin.ToCharArray();
+            for (int i = 0; i < 8; i++)
+            {
+                harfler[i] = karakterler[i].ToString();
+            }
+            //8'in 3lü kombinasyonundan başlayarak 8'in 8li kombinasyonuna kadar gidiyor
+            OrtakIslem();
+        }
+
+        private void veriCekBtn_Click(object sender, EventArgs e)//Entity framework kullanarak veritabanından kelimeleri çeker
+        {
+            string kelime;
+            var liste = from k in ent.kelimeler
+                        orderby k.words ascending
+                        select new {
+                        kelime = k.words
+                        };
+
+            dataGridView1.DataSource = liste.ToList();
+        }
+
+        private void listeleBtn_Click(object sender, EventArgs e)//Listbox1 ve Datagridviewdaki verileri tek tek karşılaştırır aynı olanları Listbox3'e atar
+        {
+            listBox3.Items.Clear();
+            int listesayisi1 = int.Parse(listBox1.Items.Count.ToString());
+            int verilistesayisi = int.Parse(dataGridView1.RowCount.ToString());
+
+            for (int i=0; i < listesayisi1; i++) {
+                for (int j=0; j < verilistesayisi; j++) {
+                    if (listBox1.Items[i].ToString() == dataGridView1.Rows[j].Cells["kelime"].Value.ToString()) 
+                    {
+                        string veri = listBox1.Items[i].ToString();
+                        int c = veri.Count();
+                        switch (c) {
+                            case 3:
+                                listBox3.Items.Add(dataGridView1.Rows[j].Cells["kelime"].Value.ToString() + ": 3 PUAN");
+                                break;
+                            case 4:
+                                listBox3.Items.Add(dataGridView1.Rows[j].Cells["kelime"].Value.ToString() + ": 4 PUAN");
+                                break;
+                            case 5:
+                                listBox3.Items.Add(dataGridView1.Rows[j].Cells["kelime"].Value.ToString() + ": 5 PUAN");
+                                break;
+                            case 6:
+                                listBox3.Items.Add(dataGridView1.Rows[j].Cells["kelime"].Value.ToString() + ": 7 PUAN");
+                                break;
+                            case 7:
+                                listBox3.Items.Add(dataGridView1.Rows[j].Cells["kelime"].Value.ToString() + ": 9 PUAN");
+                                break;
+                            case 8:
+                                listBox3.Items.Add(dataGridView1.Rows[j].Cells["kelime"].Value.ToString() + ": 11 PUAN");
+                                break; 
+                        }
+                    }
+                }
+            }
+            if (listBox3.Items.Count > 0)
+            {
+                MessageBox.Show("Anlamlı kelimeler listelendi!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Veritabanında anlamlı kelime bulunamadı!", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void yeniOyunBtn_Click(object sender, EventArgs e)//Her şeyi sıfırlar
+        {
+            listBox1.Items.Clear();
+            listBox3.Items.Clear();
+            dataGridView1.DataSource = null;
+            harflerTxt.Text = "";
+        }
+
+        public void AyniOlanlariSil()
+        {
+            foreach (string item in listBox1.Items) //listBox1 deki itemları tek tek dolaş ve bunların adına item de.
+            {
+                if (eleman.Contains(item) == false) //eğer eleman (yani sonsuz dizimiz) elemanları içermiyorsa
+                {
+                    eleman.Add(item); //sonsuz diziye itemları ekle
+                }
+            }
+            listBox1.Items.Clear(); //listBox'ı temizle
+            listBox1.Items.AddRange(eleman.ToArray()); //listBox'a eleman dizinin elemanlarını ekle
+        }//listbox1'de oynı olan değerleri siler
+
+        public void OrtakIslem()
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                var kelimeler = Kombinasyon.Combinations(karakterler, r);
+
+                foreach (var item in kelimeler)
+                {
+                    x = string.Join("", item.ToArray());
+                    listBox1.Items.Add(x);
+                }
+                r++;
+            }
+            foreach (var p in Permutasyon(harflerTxt.Text))
+            {
+                listBox1.Items.Add($"{p}");
+            }
+            AyniOlanlariSil();
+        }//Rastgele ve elle eklenen işlemlerde ortak olanları bir yerde topladım
+
+        public static IEnumerable<string> Permutasyon(string source)
+        {
+            if (source.Length == 1) return new List<string> { source };
+
+            var permutations = from z in source
+                               from p in Permutasyon(new String(source.Where(x => x != z).ToArray()))
+                               select z + p;
+            return permutations;
+        }
+    }
+    public static class Kombinasyon
+    {
+        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> x, int k)
+        {
+            return k == 0 ? new[] { new T[0] } : x.SelectMany((e, i) => x.Skip(i + 1).Combinations(k - 1).Select(c => (new[] { e }).Concat(c)));
+        }
+    }
+}
